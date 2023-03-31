8abc9e2acd7e35ef31540774d58f16a998207d01
YouTube-TW-MAIN <120999573+YouTube-TW-MAIN@users.noreply.github.com>
2023-03-31 08:46:43 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    API_ID = int(os.environ.get("API_ID", "15077074"))
+    API_HASH = os.environ.get("API_HASH", "5707eaedbdd57f22e7b7cddd5ad7db6e)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", 6151505647:AAGLewcnDAFd537Fc5VbLet1X41g0eGSyzQ)
+    STRING_SESSION = os.environ.get("STRING_SESSION", 1BVtsOLgBuzeHbX-05l26kWv9l7lLLvib9bZRBqtr7RPJPaNtoZxjC25MqZTttUCHmbKMU4PGZu4Y_0AAXGkwuXTGfAo6KJut3wFEYBqg0abNZ0AhlqFWUlIcd-BFk3nCX4W3fD6jyTqhkKbeUxqZAFYcZWoG_SMLfmc0QvKRc8LOGPrsGDwoOV7IHt0P8gS7PdNgl794plPzKZHNTSbJw8pse8BQ6xMjt7QEB4bx-o4b0tqIc8mg0_-Nt2ooMzxFqzbDkwQgV0QMA1R4hhpvnN90EZn7Tzl9j5wb4pRIwxCUILKHQIM2WSaXglqIXmk5FG67VdYzOuemLzoIje-hn8gYGBZrUdQ=)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
